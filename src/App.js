5f95dd39d7b68ab9909adcae3f70ff474aea28f3
vithiya <86958980+vithiyanwsdb@users.noreply.github.com>
2023-08-03 12:57:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Home from './Home.js';
+import PostPage from './PostPage.js';
+import NewPost from './NewPost.js';
+import About from './About.js';
+import Missing from './Missing.js';
+import Footer from './Footer.js';
+import Post from './Post.js';
+import { Routes,Route, Link } from 'react-router-dom';
+import PostLayout from './PostLayout';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/newPost">NewPost</Link></li>
+          <li><Link to="/postpage">PostPage</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/newPost" element={<NewPost/>}/>
+          <Route path="/postpage" element={<PostLayout/>}>
+              <Route index element={<PostPage/>}/>
+              <Route path=":id" element={<Post/>}/>
+              <Route path="newpost" element={<NewPost/>}/>
+          </Route>
+          
+          <Route path="*" element={<Missing/>}/>
+      </Routes>
+    {/*  <Header />
+     <Nav />
+     <Home />
+     <NewPost />
+     <PostPage />
+     <About />
+     <Missing />
+     <Footer /> */}
     </div>
   );
 }
